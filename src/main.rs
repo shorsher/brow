a67f2c9a6d1a653bdd9193ba0fcb16a9da87915f
Alex Shorsher <alex.shorsher@gmail.com>
2019-09-30 15:56:20 -0400

    adding user input, testing with docker inspect

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+extern crate clap;
+
 use std::process::Command;
+use clap::{Arg, App};
 
 fn main() {
+    let matches = App::new("brow")
+        .version("0.1")
+        .author("Alex Shorsher <alex.shorsher@gmail.com>")
+        .about("enter your docker containers :)")
+        .arg(Arg::with_name("id")
+            .required(true)
+            .index(1))
+        .get_matches();
+
+
+    let docker_id = matches.value_of("id").unwrap();
+
+
     let mut docker_list = Command::new("docker");
-    docker_list.arg("ps");
+    docker_list.arg("inspect")
+               .arg(docker_id);
     docker_list.status()
                .expect("ls command failed to start");
 }
